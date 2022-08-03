a61d8c51ed85fa4d52135833d14bf23c0178cdc9
Vlad Koblay <vkchabraww@gmail.com>
2022-08-03 11:49:44 +0300

    initial

---
+++ src/Services/Api.js
@@ -0,0 +1,4 @@
+const exampleFetch =
+  'https://api.themoviedb.org/3/movie/550?api_key=16b99e0971797b79067f0be2bdb79de1';
+
+export default exampleFetch;
---
+++ src/Services/ApiKey.js
@@ -0,0 +1,3 @@
+const API_KEY = '16b99e0971797b79067f0be2bdb79de1';
+
+export default API_KEY;
